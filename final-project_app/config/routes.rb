b4cbfe98b1462d7fe29ecfe0db46794e7b7ef736
laurakathleen <laurakwalley@gmail.com>
2016-10-26 18:05:45 -0700

    gets routes up for logs edit, update, delete

--- final-project_app/config/routes.rb
+++ final-project_app/config/routes.rb
@@ -4,12 +4,12 @@ Rails.application.routes.draw do
 
   get '/teams/:team_id', to: 'teams#show', as: 'team'
 
-
   get '/teammates/new', to: 'teammates#new', as: 'new_teammate'
   post '/teammates', to: 'teammates#create'
   get '/teammates/:teammate_id', to: 'teammates#show', as: 'teammate'
 
-  get 'teammates/:teammate_id/logs/new', to: 'logs#new', as: 'new_log'
+  get '/teammates/:teammate_id/logs', to: 'logs#index', as: 'logs'
+  get '/teammates/:teammate_id/logs/new', to: 'logs#new', as: 'new_log'
   post '/teammates/:teammate_id/logs', to: 'logs#create'
   get '/teammates/:teammate_id/logs/:log_id', to: 'logs#show', as: 'log'
   get '/teammates/:teammate_id/logs/:log_id/edit', to: 'logs#edit', as: 'edit_log'
