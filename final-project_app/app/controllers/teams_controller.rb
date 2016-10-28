90e3003751721686f838841fb12d65dedbb94e07
laurakathleen <laurakwalley@gmail.com>
2016-10-27 18:32:44 -0700

    gets form for new team and redirect working

--- final-project_app/app/controllers/teams_controller.rb
+++ final-project_app/app/controllers/teams_controller.rb
@@ -5,4 +5,20 @@ class TeamsController < ApplicationController
 		@team = Team.find_by_id(team_id)
 		@teammate = session[:teammate_id]
 	end
+
+	def new
+		@team = Team.new
+	end
+
+	def create
+		@team = Team.new(team_params)
+		@team.save
+		redirect_to @team
+	end
+
+	private
+
+	def team_params
+		params.require(:team).permit(:name, :branch, :months, :start_location, :end_location)
+	end
 end
